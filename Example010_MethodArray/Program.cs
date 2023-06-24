15b7d57c4f958c9aa9695483b7a473980b5e790e
Andrey Khokhlov <kron-k@ya.ru>
2023-06-24 14:10:26 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] myArray = { 1, 12, 31, 4, 18, 16, 17, 18 };
+
+int n = myArray.Length;
+
+int find = 18;
+int index = 0;
+
+while(index < n)
+{
+    if(myArray[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
